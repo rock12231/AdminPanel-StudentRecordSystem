00fb4e7ae3eab530d5cf5b8a605071989e76512c
rock12231 <37548802+rock12231@users.noreply.github.com>
2023-01-26 08:15:35 +0530

    add user added

--- Account/views.py
+++ Account/views.py
@@ -4,12 +4,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from Account.models import StudentAttandance
 from django.contrib.auth.models import User
-import random
 import datetime
-import pytz
-from django.utils import timezone
 from django.conf import settings
 from django.utils.timezone import make_aware
+from django.db.models import Count
+from django.contrib import messages
 
 naive_datetime = datetime.datetime.now()
 naive_datetime.tzinfo
@@ -18,7 +17,7 @@ settings.TIME_ZONE
 aware_datetime = make_aware(naive_datetime)
 aware_datetime.tzinfo 
 
-Names = ['Beautiful Olinger', 'Maleyah Cothran', 'Mercedes Comer', 'Yanuel Foor', 'Dalia Munson', 'Justus Stribling', 'Ammy Garduno', 'Sicily Krantz', 'Austynn Turgeon', 'Loic Thiessen', 'Gryphon Stork', 'Sohum Hanke', 'Zariya Sandberg', 'Rishan Raphael', 'Marko Hardman', 'Aadi Sim', 'Ellis Diehl', 'Vail Natale', 'Kayana Villalba', 'Jayne Loftus', 'Ihsan Hovis', 'October Brockway', 'Hernan Howes', 'Shneur Meece', 'Zola Menard', 'Audric Schmucker', 'Jaziyah Nowicki', 'Taha Rohrer', 'Devion Graybill', 'Mayeli Geis', 'Boone Arevalo', 'Ryett Christner', 'Jayce Greene', 'Kolsen Rupe', 'Rivan Tennison', 'Jasmyn Snipes', 'Quinlan Stamm', 'Chris Cleveland', 'Everest Helman', 'Rylin Czarnecki', 'Kruz Christianson', 'Violett Godsey', 'Haven Tanner', 'Mahalia Gregor', 'Sholom Weinberger', 'Grettel Hock', 'Kaine South', 'Tariq Bergstrom', 'Rey Coe', 'Therese Baskin', 'Mariela Cyr', 'Helaina Madson', 'Karianna Bridwell', 'Jaykob Vital', 'Mendy Jacobi', 'Noemie Bono', 'Joslynn Lerma', 'Jovanni Banuelos', 'Dashiell Carrier', 'Allie Mckenzie', 'Moussa Ito', 'Makynlee Vicente', 'Terran Mitchum', 'Janel Nowakowski', 'Trenton Beard', 'Taleah Shinn', 'Randolph Etter', 'Makiyah Coyne', 'Rowyn Bloodworth', 'Armand Herd', 'Aariz Dionne', 'Maddy Mikesell', 'Jaia Brisson', 'Carma Frisbie', 'Karmello Lavergne', 'Kaitlynn Osorio', 'Lucianna Sisco', 'Kaison Thorpe', 'Ivie Towns', 'Khari Omeara', 'Edison Helton', 'Yisroel Hearn', 'Kristy Boring', 'Azaleah Farah', 'Mercer Hannigan', 'Andrew Hill', 'Akhil Rodas', 'Jamin Stanek', 'Maylin Brandenburg', 'Romel Scholz', 'Josefina Munguia', 'Cree Suzuki', 'Brithany Lazar', 'Deegan Harp', 'Gretel Mickey', 'Delphine Hatley', 'Maiya Cornwell', 'Kameren Files', 'Kobi Torrey', 'Tyson Christian', 'Aleeyah Staten', 'Karely Sessions', 'Arian Wroblewski', 'Sohan Goodale', 'Noreen Navarrette', 'Riggs Trainor', 'Aahan Vancamp', 'Azaria Blount', 'Sayuri Landa', 'Hillary Shanks', 'Ryden Canada', 'Letty Hammons', 'Treasure Fagan', 'Brantley Hershey', 'Lyric Chase', 'Brisa Vallejo', 'Taraji Hooks', 'Leobardo Dufresne', 'Anayah Brito', 'Chloe Peterson', 'Nyla Andrade', 'Aayush Mosier', 'Amorie Moretti', 'Lexie Gee', 'Carmyn Beaman', 'Dash Timmons', 'Austen Libby', 'Alyana Pedraza', 'Braylon Huynh', 'Eddison Tarin', 'Kallan Leonardo', 'Nami Dey', 'Melisa Tarr', 'Evyn Philip', 'Shayaan Bunton', 'Amaru Depasquale', 'Jenai Caffrey', 'Elie Delapena', 'Benjamin Moore', 'Aydrian Belisle', 'Adalind Frame', 'Maryana Reveles', 'Kaycen Cravens', 'Dashawn Mccallum', 'Johnnie Schiff', 'Shilah Peachey', 'Nori Vest', 'Lennon Goff', 'Aniyla Dinkins', 'Tysen Bodnar', 'Zayra Rude', 'Norberto Sealy', 'Imogen Dorman', 'Eugenia Korte', 'Samuel Allen', 'Ryder Boyd', 'Lillyan Denham', 'Chancellor Hutchings', 'Tayden Landrum', 'Tate Cisneros', 'Daysi Sill', 'Llewyn Desmarais', 'Easton Ellis', 'Asiyah Ballesteros', 'Scotlynn Backer', 'Haydee Trusty', 'Rain Peter', 'Chany Brotherton', 'Nadiyah Privett', 'Ishmael Hopson', 'Lamiya Alejo', 'Ben Miner', 'Jhene Almonte', 'Rajon Spiker', 'Yusuf Madison', 'Dixie Wiseman', 'Rosalina Hostetler', 'Yessica Sundberg', 'Harmony Francis', 'Santos Woodson', 'Yelena Kight', 'Subhan Harner', 'Favian Beecher', 'Darron Comfort', 'Sirius Rhyne', 'Xoe Binion', 'Cruze Shivers', 'Saint Cavender', 'Markeith Mathewson', 'Graison Hallam', 'Kashton Deluca', 'Ayush Falls', 'Mabel Connolly', 'Harmonie Holton', 'Dawsyn Laurence', 'Marelyn Kiss', 'Dalary Coyle', 'Braelynn Dudley', 'Katherin Burgin', 'Ayumi Brockett']
+# Names = ['Beautiful Olinger', 'Maleyah Cothran', 'Mercedes Comer', 'Yanuel Foor', 'Dalia Munson', 'Justus Stribling', 'Ammy Garduno', 'Sicily Krantz', 'Austynn Turgeon', 'Loic Thiessen', 'Gryphon Stork', 'Sohum Hanke', 'Zariya Sandberg', 'Rishan Raphael', 'Marko Hardman', 'Aadi Sim', 'Ellis Diehl', 'Vail Natale', 'Kayana Villalba', 'Jayne Loftus', 'Ihsan Hovis', 'October Brockway', 'Hernan Howes', 'Shneur Meece', 'Zola Menard', 'Audric Schmucker', 'Jaziyah Nowicki', 'Taha Rohrer', 'Devion Graybill', 'Mayeli Geis', 'Boone Arevalo', 'Ryett Christner', 'Jayce Greene', 'Kolsen Rupe', 'Rivan Tennison', 'Jasmyn Snipes', 'Quinlan Stamm', 'Chris Cleveland', 'Everest Helman', 'Rylin Czarnecki', 'Kruz Christianson', 'Violett Godsey', 'Haven Tanner', 'Mahalia Gregor', 'Sholom Weinberger', 'Grettel Hock', 'Kaine South', 'Tariq Bergstrom', 'Rey Coe', 'Therese Baskin', 'Mariela Cyr', 'Helaina Madson', 'Karianna Bridwell', 'Jaykob Vital', 'Mendy Jacobi', 'Noemie Bono', 'Joslynn Lerma', 'Jovanni Banuelos', 'Dashiell Carrier', 'Allie Mckenzie', 'Moussa Ito', 'Makynlee Vicente', 'Terran Mitchum', 'Janel Nowakowski', 'Trenton Beard', 'Taleah Shinn', 'Randolph Etter', 'Makiyah Coyne', 'Rowyn Bloodworth', 'Armand Herd', 'Aariz Dionne', 'Maddy Mikesell', 'Jaia Brisson', 'Carma Frisbie', 'Karmello Lavergne', 'Kaitlynn Osorio', 'Lucianna Sisco', 'Kaison Thorpe', 'Ivie Towns', 'Khari Omeara', 'Edison Helton', 'Yisroel Hearn', 'Kristy Boring', 'Azaleah Farah', 'Mercer Hannigan', 'Andrew Hill', 'Akhil Rodas', 'Jamin Stanek', 'Maylin Brandenburg', 'Romel Scholz', 'Josefina Munguia', 'Cree Suzuki', 'Brithany Lazar', 'Deegan Harp', 'Gretel Mickey', 'Delphine Hatley', 'Maiya Cornwell', 'Kameren Files', 'Kobi Torrey', 'Tyson Christian', 'Aleeyah Staten', 'Karely Sessions', 'Arian Wroblewski', 'Sohan Goodale', 'Noreen Navarrette', 'Riggs Trainor', 'Aahan Vancamp', 'Azaria Blount', 'Sayuri Landa', 'Hillary Shanks', 'Ryden Canada', 'Letty Hammons', 'Treasure Fagan', 'Brantley Hershey', 'Lyric Chase', 'Brisa Vallejo', 'Taraji Hooks', 'Leobardo Dufresne', 'Anayah Brito', 'Chloe Peterson', 'Nyla Andrade', 'Aayush Mosier', 'Amorie Moretti', 'Lexie Gee', 'Carmyn Beaman', 'Dash Timmons', 'Austen Libby', 'Alyana Pedraza', 'Braylon Huynh', 'Eddison Tarin', 'Kallan Leonardo', 'Nami Dey', 'Melisa Tarr', 'Evyn Philip', 'Shayaan Bunton', 'Amaru Depasquale', 'Jenai Caffrey', 'Elie Delapena', 'Benjamin Moore', 'Aydrian Belisle', 'Adalind Frame', 'Maryana Reveles', 'Kaycen Cravens', 'Dashawn Mccallum', 'Johnnie Schiff', 'Shilah Peachey', 'Nori Vest', 'Lennon Goff', 'Aniyla Dinkins', 'Tysen Bodnar', 'Zayra Rude', 'Norberto Sealy', 'Imogen Dorman', 'Eugenia Korte', 'Samuel Allen', 'Ryder Boyd', 'Lillyan Denham', 'Chancellor Hutchings', 'Tayden Landrum', 'Tate Cisneros', 'Daysi Sill', 'Llewyn Desmarais', 'Easton Ellis', 'Asiyah Ballesteros', 'Scotlynn Backer', 'Haydee Trusty', 'Rain Peter', 'Chany Brotherton', 'Nadiyah Privett', 'Ishmael Hopson', 'Lamiya Alejo', 'Ben Miner', 'Jhene Almonte', 'Rajon Spiker', 'Yusuf Madison', 'Dixie Wiseman', 'Rosalina Hostetler', 'Yessica Sundberg', 'Harmony Francis', 'Santos Woodson', 'Yelena Kight', 'Subhan Harner', 'Favian Beecher', 'Darron Comfort', 'Sirius Rhyne', 'Xoe Binion', 'Cruze Shivers', 'Saint Cavender', 'Markeith Mathewson', 'Graison Hallam', 'Kashton Deluca', 'Ayush Falls', 'Mabel Connolly', 'Harmonie Holton', 'Dawsyn Laurence', 'Marelyn Kiss', 'Dalary Coyle', 'Braelynn Dudley', 'Katherin Burgin', 'Ayumi Brockett']
 
     
 class HomeView(LoginRequiredMixin,View):
@@ -27,23 +26,22 @@ class HomeView(LoginRequiredMixin,View):
     def get(self, request):
         totalstudent = User.objects.all().exclude(is_superuser=True).count()
         student = StudentAttandance.getByMonth(1,2022)
+        
         preset_students = student.filter(student_attandance=True).count()
         absebt_students = student.filter(student_attandance=False).count()
+        
         total_record = preset_students + absebt_students
         start_date = student.order_by('created_at').first().created_at
         last_date = student.order_by('created_at').last().created_at
-        listdata = []
-        for i in range(1,30):
-            OneDayPresentStudent = student.filter(updated_at__day=i,student_attandance=True).count()
-            listdata.append(OneDayPresentStudent)
+        
+        result_list = student.filter(student_attandance=True).values('updated_at__day').annotate(dcount=Count('updated_at__day')).order_by()        
+        count_list =[x['dcount'] for x in result_list]
         OneMonthStudent = student.values()
-        for i in OneMonthStudent:
-            if i['student_attandance'] == True:
-                i['student_attandance'] = "Present"
-            else:
-                i['student_attandance'] = "Absent"
-            i['updated_at'] = i['updated_at'].strftime("%d-%m-%Y %H:%M:%S")
-            i['created_at'] = i['created_at'].strftime("%d-%m-%Y %H:%M:%S")
+        
+        for i in range(len(OneMonthStudent)):
+            OneMonthStudent[i]['created_at'] = OneMonthStudent[i]['created_at'].strftime("%d-%m-%Y %H:%M:%S")
+            OneMonthStudent[i]['updated_at'] = OneMonthStudent[i]['updated_at'].strftime("%d-%m-%Y %H:%M:%S")
+            OneMonthStudent[i]['student_attandance'] = "Present" if OneMonthStudent[i]['student_attandance'] else "Absent"
             
         context = {
             "total":totalstudent,
@@ -52,15 +50,30 @@ class HomeView(LoginRequiredMixin,View):
             "total_record":total_record,
             "start":start_date,
             "last":last_date,
-            "data" :[listdata,[i for i in range(1,30)]],
+            "data" :[count_list,[i for i in range(1,30)]],
             "month":list(OneMonthStudent)
         }
+        
         return render(request, 'Account/index.html',context)
     def post(self, request):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        try:
+            data = User.objects.create(username=name, email=email, first_name=first_name, last_name=last_name, password="12345678")
+            data.save()
+            messages.success(request, 'Student Added Successfully')
+            print("data saved >>>>>>>")
+        except:
+            messages.error(request, 'Student Already Exist')
         return render(request, 'Account/index.html')
     
 class LoginView(View,LoginRequiredMixin, UserPassesTestMixin):
     def get(self, request):
+        
+        # Base method with no type specified
+       
         # for i in range(1,12):
         #     for j in range(1,28):
         #         for name in Names:
